e6ce8441912115ccbce23bf4815d0f909511b339
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-02-26 10:40:36 -0500

    Create very basic version of site

--- simplesite.py
+++ simplesite.py
@@ -1,1 +1,12 @@
 from flask import flask
+
+app=Flask(__name__)
+
+# For the homepage
+@app.route('/')
+
+def home():
+    return "This is a website!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
