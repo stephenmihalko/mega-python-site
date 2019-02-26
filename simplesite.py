8c65f09dcb669d4e8e4e472bf17a90c57fef02fc
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-02-26 10:50:58 -0500

    Added render_template to return an HTML document

--- simplesite.py
+++ simplesite.py
@@ -1,4 +1,5 @@
-from flask import Flask
+# render_template accesses an HTML file stored in the system and displays it
+from flask import Flask, render_template
 
 # This "__name__" gets the name of the Python script.
 app=Flask(__name__)
@@ -7,7 +8,8 @@ app=Flask(__name__)
 @app.route('/')
 
 def home():
-    return "This is a website!"
+    # This needs to be a real thing in the "templates" folder.
+    return render_template("home.html")
 
 # When you execute a script, this is true.
 # When  you import a script, __name__ is given the name of the script.
