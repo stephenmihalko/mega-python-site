ba849219ef0b879d39f3bec5edf34912a001d0ca
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-02-26 10:47:12 -0500

    Add actual site functionality

--- simplesite.py
+++ simplesite.py
@@ -1,5 +1,6 @@
-from flask import flask
+from flask import Flask
 
+# This "__name__" gets the name of the Python script.
 app=Flask(__name__)
 
 # For the homepage
@@ -8,5 +9,7 @@ app=Flask(__name__)
 def home():
     return "This is a website!"
 
+# When you execute a script, this is true.
+# When  you import a script, __name__ is given the name of the script.
 if __name__ == "__main__":
     app.run(debug=True)
